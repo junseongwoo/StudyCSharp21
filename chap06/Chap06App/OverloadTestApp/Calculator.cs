04c200530179a1f0b39dc9aeda12aa42e3171220
junseongwoo <wnstjd4852@gmail.com>
2021-02-24 10:42:42 +0900

    0224 중간 커밋

---
+++ chap06/Chap06App/OverloadTestApp/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadTestApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("계산기!");
+
+            int x = Calculator.Plus(3, 4);
+            Console.WriteLine($"3 + 4 = {x}");
+
+            int total = 0;
+            total = Sum(1, 2);
+
+            Console.WriteLine($"10까지의 합은 {Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}");
+
+            int[] arrs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };  // 배열을 만들어서 바로 적용함 
+            Console.WriteLine($"10까지의 합은 {Sum(arrs)}"); 
+        }
+
+        private static int Sum(params int[] args)  //가변길이 매개변수 params
+        {
+            int result = 0;
+
+            foreach (var arg in args)
+            {
+                result += arg;
+            }
+            return result;
+        }
+
+        private static int Plus(params int[] args)
+        {
+            int result = 0;
+
+            foreach (var arg in args)
+            {
+                result += arg; 
+            }
+            return result;
+        }
+    }
+}
--- chap06/Chap06App/ProfileTestApp/Program.cs
+++ chap06/Chap06App/ProfileTestApp/Program.cs
@@ -10,21 +10,30 @@ namespace ProfileTestApp
     {
         static void Main(string[] args)
         {
-            PrintProfile("성명건", "010-1234-1234");
+            PrintProfile("성명건", "010-6683-7732");
 
-            if (PrintProfile(null, "010-1234-1234") = -1 )
+            if (PrintProfile(null, "010-1111-2222") == -1)
             {
                 Console.WriteLine("프로필 출력시 오류가 발생했습니다.");
             }
+
+            /*PrintProfile(phone: "010-1234-1234", name: "홍길동");
+            PrintProfile(phone: "010-1234-1234", name: "홍길동");*/
+
+            PrintProfile("우준성");
         }
-        public static void PrintProfile(string name, string phone)
+
+        public static int PrintProfile(string name, string phone = "입력 하지 않음") // defult 파라미터는 무조건 뒤에서 부터 넣어야한다.
         {
             if (string.IsNullOrEmpty(name))
             {
                 Console.WriteLine("이름을 정확히 입력하세요.");
                 return -1;
             }
-            Console.WriteLine($"이름 : {name}, 폰 번호 : {phone}");
+
+            // 프로필 출력
+            Console.WriteLine($"이름 : {name}, 폰번호 : {phone}");
+            return 0;
         }
     }
 }
