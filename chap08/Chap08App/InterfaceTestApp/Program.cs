7ea0286e55e8c13f60cbbfb1882c28221bcdbdb1
junseongwoo <wnstjd4852@gmail.com>
2021-02-26 15:53:19 +0900

    0226 8장 인터페이스 커밋

---
+++ chap08/Chap08App/InterfaceTestApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTestApp
+{
+    // 다중 상속으로 하면 Ride를 어떤것을 써야하는지 인식하지 못해 오류를 발생한다.
+    // Interface를 사용하여 해결한다.
+    /* class Ridable // 탈것
+     {
+         public virtual void Ride()
+         {
+             Console.WriteLine("탈것!");
+         }
+     }*/
+    interface ICar
+    {
+        void Run();
+        void Ride();
+    }
+
+    interface IPlane
+    {
+        void Fly();
+        void Ride();
+
+    }
+    class DroneCar : IPlane, ICar
+    {
+        public void Fly()
+        {
+            Console.WriteLine("드론카 날다");
+        }
+
+        public void Ride()
+        {
+            Run();
+            Fly();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("드론카 달리다.");
+        }
+    }
+    /*class car : Ridable
+    {
+        public void Run()
+        {
+            Console.WriteLine("달려!");
+        }
+        
+        public override void Ride()
+        {
+            base.Ride();
+            this.Run();
+        }
+    }*/
+
+    /*class AirPlane : Ridable // 상속 
+    {
+        public void Fly ()
+        {
+            Console.WriteLine("날아!!");
+        }
+
+        public override void Ride()
+        {
+            base.Ride();
+            this.Fly();
+        }
+    }*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DroneCar dreamCar = new DroneCar();
+            dreamCar.Ride();
+        }
+    }
+}
---
+++ chap08/Chap08App/LoggerTestAPP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerTestApp
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} : {message}");
+        }
+    }
+    interface IFormattableLogger : ILogger
+    {
+        void WriteLog(string format, params Object[] args);
+    }
+
+    class ConsoleFormatLogger : IFormattableLogger
+    {
+        public void WriteLog(string format, params object[] args)
+        {
+            string message = String.Format(format, args);
+            Console.WriteLine($"{DateTime.Now.ToLocalTime()} / {message}");
+        }
+
+        public void WriteLog(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleLogger 테스트");
+            ConsoleLogger logger = new ConsoleLogger();
+            logger.W
+        }
+    }
+}
