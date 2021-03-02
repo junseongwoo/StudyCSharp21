9788adb6e71126018bae344c5ca309802a297648
junseongwoo <wnstjd4852@gmail.com>
2021-03-02 12:30:13 +0900

    0302 11장커밋

--- chap11/Chap11App/GenericCopyArrayApp/Program.cs
+++ chap11/Chap11App/GenericCopyArrayApp/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chap11App
+namespace GenericCopyArrayApp
 {
-    class Program  // 일반화를 하게 되면 타입이 다른 같은 일들을 하나로 만들어 편하게 사용한다. 
+    class Program
     {
         static void Main(string[] args)
         {
@@ -31,39 +31,11 @@ namespace Chap11App
             {
                 Console.WriteLine(item);
             }
-
-            float[] source3 = { 1.1f, 2.2f, 3.3f, 4.4f, 5.5f };
-            float[] target3 = new float[source2.Length];
-
-            CopyArray(source3, target3);
-
-            Console.WriteLine("배열복사 3");
-            foreach (var item in target3)
-            {
-                Console.WriteLine(item);
-            }
-
         }
 
-        private static void CopyArray(float[] source, float[] target)
+        private static void CopyArray<T>(T[] source, T[] target)
         {
             for (int i = 0; i < source.Length; i++)
-            {
-                target[i] = source[i];
-            }
-        }
-
-        private static void CopyArray(string[] source, string[] target)
-        {
-            for(int i = 0; i < source.Length; i++)
-            {
-                target[i] = source[i];
-            }
-        }
-
-        private static void CopyArray(int[] source, int[] target)
-        {
-            for(int i= 0; i < source.Length; i++)
             {
                 target[i] = source[i];
             }
---
+++ chap11/Chap11App/GenericListApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListApp
+{
+    class Program
+    {
+        //실무에서 가장 많이 사용되는 컬렉션1
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+            list.Reverse();
+
+            Console.WriteLine("역정렬");
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
