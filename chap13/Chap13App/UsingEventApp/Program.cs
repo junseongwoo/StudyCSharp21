ae4d140e534f1e1dd5c9a064a016e25701125124
junseongwoo <wnstjd4852@gmail.com>
2021-03-03 10:26:38 +0900

    13장 대리자, 이벤트 커밋

---
+++ chap13/Chap13App/UsingEventApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingEventApp
+{
+    delegate void EventHandler(string message);
+
+    class CustomNotifier
+    {
+        public event EventHandler SomethingHappend;
+
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+
+            if (temp != 0 && temp % 3 ==0)
+            {
+                SomethingHappend($"{number} : 짝!");
+            }
+            else
+            {
+                SomethingHappend($"{number}"); // 이벤트를 사용
+            }
+        }
+    }
+    class Program
+    {
+        public static void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이벤트 사용!");
+            CustomNotifier notifier = new CustomNotifier();
+            notifier.SomethingHappend += new EventHandler(MyHandler); // 이벤트를 내가만든 로직이 있는 메서드랑 연결
+
+            for (int i = 1; i < 100; i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+    }
+}
