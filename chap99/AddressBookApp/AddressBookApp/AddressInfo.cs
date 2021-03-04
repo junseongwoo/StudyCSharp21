55ea630ec271c2f22a847ca0f7d10134c36d2f95
junseongwoo <wnstjd4852@gmail.com>
2021-03-04 16:13:29 +0900

    주소록 프로그램 커밋

---
+++ chap99/AddressBookApp/AddressBookApp/AddressInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookApp
+{
+    class AddressInfo
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ chap99/AddressBookApp/AddressBookApp/DataFileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookApp
+{
+    class DataFileManager
+    {
+        // 텍스트 파일에서 저장된 주소록을 불러온다(로드)
+        const string dataFileName = "address.dat";
+        // D:\sources\GitRepository\StudyCSharp21\chap99\AddressBookApp\AddressBookApp\bin\Debug\netcoreapp3.1
+
+        public List<AddressInfo> ReadData()
+        {
+            var listResult = new List<AddressInfo>();
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName;
+            var sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            while (sr.EndOfStream == false)
+            {
+                var temp = sr.ReadLine();
+                // temp 데이터를 잘라 manager.listAddress 할당
+                var splits = temp.Split("|");
+                listResult.Add(new AddressInfo() { Name = splits[0], Phone = splits[1], Address = splits[2] });
+            }
+            sr.Close();
+
+            return listResult;
+        }
+        public void WriteData(List<AddressInfo> list)
+        {
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName;
+            // 주소록을 다시 파일에 씀 
+            var sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write));
+
+            if (list.Count > 0)
+            {
+                foreach (var item in list)
+                {
+                    sw.WriteLine($"{item.Name}|{item.Phone}|{item.Address}");
+                }
+            }
+
+            sw.Close();
+        }
+    }
+}
